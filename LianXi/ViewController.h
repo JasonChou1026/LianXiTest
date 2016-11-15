2ee5a21cd30ced2e85f73cf05a69342897ab8799
Jason <jason@192.168.0.111>
2016-11-15 11:46:33 +0800

    Initial Commit

---
+++ LianXi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LianXi
+//
+//  Created by Jason on 16/11/15.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LianXi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LianXi
+//
+//  Created by Jason on 16/11/15.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
